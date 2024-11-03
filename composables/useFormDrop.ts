64826bf3489a2ee1e063e919428cf4d0f4410b59
ai-strings <adrstrings@gmail.com>
2024-11-03 17:01:49 +0300

    +add drag and drop composable

---
+++ composables/useFormDrop.ts
@@ -0,0 +1,169 @@
+export const useFormDrop = ({
+  constructorAreaSelector = "",
+  customDropZoneSelector = "",
+}: {
+  constructorAreaSelector: string;
+  customDropZoneSelector: string;
+}) => {
+  const elDropZoneSelector = customDropZoneSelector || "#drop-insert-place";
+  let constructorArea;
+
+  let elDropZone: HTMLDivElement | null = null;
+
+  const formItems = useState<string[]>("formItems", () => []);
+
+  function setConstructorArea() {
+    if (process.client) {
+      constructorArea = document.querySelector(constructorAreaSelector);
+      console.log(
+        constructorArea,
+        constructorAreaSelector,
+        "setting constrArea"
+      );
+    }
+  }
+  setConstructorArea();
+
+  function getElDropZone() {
+    if (process.client) {
+      elDropZone = document.querySelector(elDropZoneSelector);
+    }
+  }
+  function createDropZoneEl() {
+    if (process.client) {
+      elDropZone = document.createElement("div");
+      elDropZone.id = "drop-insert-place";
+    }
+  }
+  function onDrag(e): void {
+    console.log(constructorArea, "constructorArea");
+    console.log(e.clientX, "onDrag");
+    console.log(e.clientY, "onDrag");
+
+    const { bottom, top, left, right } =
+      constructorArea?.getBoundingClientRect();
+    const isOutOfBounds =
+      e.clientY > bottom ||
+      e.clientY < top ||
+      e.clientX < left ||
+      e.clientX > right;
+
+    if (isOutOfBounds) {
+      getElDropZone();
+      if (!elDropZone) return;
+      elDropZone?.remove();
+    }
+  }
+  function startDrag(evt): void {
+    evt.dataTransfer.dropEffect = "move";
+    evt.dataTransfer.effectAllowed = "move";
+    evt.dataTransfer.setData("itemID", evt.target?.id);
+  }
+  function onDrop(e): void {
+    e.stopImmediatePropagation();
+    const itemID = e.dataTransfer.getData("itemID");
+
+    if (e.target.id === "constructor-drop") {
+      formItems.value.push(itemID);
+      e.target.classList.remove("active");
+    } else {
+      getElDropZone();
+      const ind = elDropZone?.dataset?.index;
+      console.log(ind);
+      elDropZone?.remove();
+      formItems.value.splice(ind, 0, itemID);
+    }
+  }
+  function onConstructorAreaDragEnter(e): void {
+    getElDropZone();
+    if (elDropZone) return;
+    e.target.classList.add("active");
+  }
+
+  function onConstructorAreaDragLeave(evt): void {
+    // let prependZone = document.querySelector("#drop-insert-place");
+    // if (prependZone) prependZone.remove();
+    evt.target.classList.remove("active");
+  }
+
+  function onComponentDragEnter(e): void {
+    if (process.client) {
+      e.preventDefault();
+      getElDropZone();
+      if (!elDropZone) {
+        createDropZoneEl();
+        constructorArea?.insertBefore(elDropZone as HTMLElement, e.target);
+      }
+    }
+  }
+  // function onComponentDragLeave(e): void {
+  //   console.log(e.target.getBoundingClientRect(), "leave");
+  //   const { bottom, top, right, left } = e.target.getBoundingClientRect();
+  //   console.log(e.clientX, e.clientY, "leave");
+  //   const isVerticalLeave =
+  //     Math.floor(bottom) - e.clientY <= 1 || Math.floor(top) - e.clientY >= 1;
+  //   const isHorizontalLeave =
+  //     Math.floor(right) - e.clientX <= 1 || Math.floor(left) - e.clientX >= 1;
+  //   if (isHorizontalLeave) {
+  //     let prependZone = document.querySelector("#drop-insert-place");
+  //     if (!prependZone) return;
+  //     prependZone.remove();
+  //   }
+  // }
+  function onComponentDragOver(e): void {
+    e.preventDefault();
+    //смотрим, куда прёт курсор: если ниже, отвязываем, если выше, то даём управление добавленному элементу
+    if (process.client) {
+      const checkDirectionVertical =
+        e.target.getBoundingClientRect().bottom -
+          e.target.getBoundingClientRect().height / 2 >
+        e.clientY
+          ? "up"
+          : "down";
+      const isOutHotizontally =
+        e.target.getBoundingClientRect().left > e.clientX ||
+        e.target.getBoundingClientRect().right < e.clientX;
+
+      // console.log(e.clientY);
+      console.log(e.target.getBoundingClientRect());
+      console.log(e.clientX);
+      getElDropZone();
+      if (!elDropZone) {
+        createDropZoneEl();
+      }
+      if (isOutHotizontally) {
+        elDropZone?.remove();
+        return;
+      }
+      if (checkDirectionVertical === "down") {
+        console.log(e.target.nextElementSibling, "e.target.nextSibling");
+        if (
+          e.target.nextElementSibling &&
+          e.target.nextElementSibling.classList?.contains(
+            "constructor-area__component"
+          )
+        ) {
+          constructorArea?.insertBefore(
+            elDropZone as HTMLElement,
+            e.target.nextElementSibling
+          );
+        } else if (!e.target.nextElementSibling) {
+          if (elDropZone) elDropZone.remove();
+          constructorArea?.classList.add("active");
+        }
+      } else {
+        constructorArea?.insertBefore(elDropZone as HTMLElement, e.target);
+      }
+    }
+  }
+  return {
+    formItems,
+    startDrag,
+    onDrop,
+    onComponentDragEnter,
+    onComponentDragOver,
+    onConstructorAreaDragEnter,
+    onConstructorAreaDragLeave,
+    onDrag,
+  };
+};
--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,12 @@
       ref="constructorArea"
       @drop="onDrop($event)"
       @dragover.prevent
-      @dragenter="onDragEnter"
-      @dragleave="onDragLeave"
+      @dragenter="onConstructorAreaDragEnter"
+      @dragleave="onConstructorAreaDragLeave"
     >
       <component
         class="constructor-area__component"
-        v-for="(item, index) in items"
+        v-for="(item, index) in formItems"
         :data-index="index"
         :is="componentsMap[item]"
         :key="`${item}-${index}`"
@@ -47,141 +47,23 @@ import BaseInput from "../components/base/BaseInput.vue";
 import Header from "../components/base/Header.vue";
 
 import type { Component } from "@vue/runtime-core";
-import { ComponentsMap } from "#build/types";
-
+import { ComponentsMap } from "@/types";
+import { useFormDrop } from "@/composables/useFormDrop";
 const componentsMap: ComponentsMap = {
   BaseInput: BaseInput,
   Header: Header,
 };
 
-const items = ref<string[]>([]);
-const constructorArea = ref<HTMLDivElement>(null);
-
-function onDrag(e): void {
-  console.log(e.clientX, "onDrag");
-  console.log(e.clientY, "onDrag");
-
-  const { bottom, top, left, right } =
-    constructorArea.value.getBoundingClientRect();
-  const isOutOfBounds =
-    e.clientY > bottom ||
-    e.clientY < top ||
-    e.clientX < left ||
-    e.clientX > right;
-
-  if (isOutOfBounds) {
-    let prependZone = document.querySelector("#drop-insert-place");
-    if (!prependZone) return;
-    prependZone.remove();
-  }
-}
-function startDrag(evt): void {
-  evt.dataTransfer.dropEffect = "move";
-  evt.dataTransfer.effectAllowed = "move";
-  evt.dataTransfer.setData("itemID", evt.target?.id);
-}
-function onDrop(evt): void {
-  evt.stopImmediatePropagation();
-  const itemID = evt.dataTransfer.getData("itemID");
-
-  if (evt.target.id === "constructor-drop") {
-    items.value.push(itemID);
-    evt.target.classList.remove("active");
-  } else {
-    const ind = evt.target.dataset.index;
-    console.log(ind);
-    items.value.splice(ind, 0, itemID);
-  }
-}
-function onDragEnter(evt): void {
-  let prependZone = document.querySelector("#drop-insert-place");
-  if (prependZone) return;
-  evt.target.classList.add("active");
-}
-
-function onDragLeave(evt): void {
-  // let prependZone = document.querySelector("#drop-insert-place");
-  // if (prependZone) prependZone.remove();
-  evt.target.classList.remove("active");
-}
-
-function onComponentDragEnter(e): void {
-  if (process.client) {
-    e.preventDefault();
-    //если prependZone уже есть, но мы на другом элементе (выше/ниже основного компонента), нужно убирать старый препенд и добавлять новый
-    let prependZone = document.querySelector("#drop-insert-place");
-    if (!prependZone) {
-      prependZone = document.createElement("div");
-      prependZone.id = "drop-insert-place";
-      //привязать drag эвенты, смотреть, куда уходим, отталкиваться от этого
-
-      // console.log(prependZone);
-      // console.log(e.target, "e.target");
-      constructorArea.value.insertBefore(prependZone, e.target);
-    }
-  }
-}
-// function onComponentDragLeave(e): void {
-//   console.log(e.target.getBoundingClientRect(), "leave");
-//   const { bottom, top, right, left } = e.target.getBoundingClientRect();
-//   console.log(e.clientX, e.clientY, "leave");
-//   const isVerticalLeave =
-//     Math.floor(bottom) - e.clientY <= 1 || Math.floor(top) - e.clientY >= 1;
-//   const isHorizontalLeave =
-//     Math.floor(right) - e.clientX <= 1 || Math.floor(left) - e.clientX >= 1;
-//   if (isHorizontalLeave) {
-//     let prependZone = document.querySelector("#drop-insert-place");
-//     if (!prependZone) return;
-//     prependZone.remove();
-//   }
-// }
-function onComponentDragOver(e): void {
-  e.preventDefault();
-  //смотрим, куда прёт курсор: если ниже, отвязываем, если выше, то даём управление добавленному элементу
-  if (process.client) {
-    const checkDirectionVertical =
-      e.target.getBoundingClientRect().bottom -
-        e.target.getBoundingClientRect().height / 2 >
-      e.clientY
-        ? "up"
-        : "down";
-    const isOutHotizontally =
-      e.target.getBoundingClientRect().left > e.clientX ||
-      e.target.getBoundingClientRect().right < e.clientX;
-
-    // console.log(e.clientY);
-    console.log(e.target.getBoundingClientRect());
-    console.log(e.clientX);
-    let prependZone = document.querySelector("#drop-insert-place");
-    if (!prependZone) {
-      prependZone = document.createElement("div");
-      prependZone.id = "drop-insert-place";
-    }
-    if (isOutHotizontally) {
-      prependZone.remove();
-      return;
-    }
-    if (checkDirectionVertical === "down") {
-      console.log(e.target.nextElementSibling, "e.target.nextSibling");
-      if (
-        e.target.nextElementSibling &&
-        e.target.nextElementSibling.classList?.contains(
-          "constructor-area__component"
-        )
-      ) {
-        constructorArea.value.insertBefore(
-          prependZone,
-          e.target.nextElementSibling
-        );
-      } else if (!e.target.nextElementSibling) {
-        if (prependZone) prependZone.remove();
-        constructorArea.value.classList.add("active");
-      }
-    } else {
-      constructorArea.value.insertBefore(prependZone, e.target);
-    }
-  }
-}
+const {
+  formItems,
+  startDrag,
+  onDrop,
+  onComponentDragEnter,
+  onComponentDragOver,
+  onConstructorAreaDragEnter,
+  onConstructorAreaDragLeave,
+  onDrag,
+} = useFormDrop({ constructorAreaSelector: "#constructor-drop" });
 </script>
 
 <style scoped></style>
