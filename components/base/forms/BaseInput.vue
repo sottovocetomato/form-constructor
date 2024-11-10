4c2e33976d740cdcd2ce8b4d821804cad14bcc76
ai-strings <adrstrings@gmail.com>
2024-11-10 17:59:25 +0300

    +add input props

--- components/base/forms/BaseInput.vue
+++ components/base/forms/BaseInput.vue
@@ -1,25 +1,45 @@
 <template>
   <input
     :type="type"
-    :name="firstname"
+    :name="name"
     :id="id"
     :placeholder="placeholder"
     :aria-label="ariaLabel"
     :class="customClass"
     :required="required"
+    :readonly="readonly"
+    :disabled="disabled"
+    :aria-invalid="ariaInvalid"
   />
+  <small v-if="ariaInvalid" id="invalid-helper">Not valid input</small>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
+interface Props {
   type: string;
-  firstname?: string;
+  name?: string;
   id?: string;
   placeholder?: string;
   ariaLabel?: string;
+  ariaInvalid?: boolean | null;
   customClass?: string;
   required?: boolean;
-}>();
+  readonly?: boolean;
+  disabled?: boolean;
+}
+
+const {
+  type = "text",
+  name = "input",
+  ariaLabel = "Text Input",
+  required = true,
+  readonly = false,
+  disabled = false,
+  ariaInvalid = null,
+  customClass = "",
+  placeholder = "Your text",
+  id = "",
+} = defineProps<Props>();
 </script>
 
 <style scoped></style>
